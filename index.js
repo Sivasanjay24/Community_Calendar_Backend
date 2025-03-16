c65d0da56b48927ba3336badf178b9570b81c355
Sivasanjay24 <sanjaysanjith2004@gmail.com>
2025-03-16 18:07:27 +0530

    get

--- index.js
+++ index.js
@@ -38,3 +38,12 @@ mdb
     }
   });
 
+  app.get('/details', async (req, res) => {
+    try {
+      const events = await Details.find(); // Fetch all events from MongoDB
+      res.json(events); // Send events as JSON response
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
